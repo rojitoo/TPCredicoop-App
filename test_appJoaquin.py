3960c65290aff800aace7624031beb2aa42b04ac
Rojo Provenzano Joaquín Lucio <jrojoprovenzano@frba.utn.edu.ar>
2023-11-30 16:10:07 -0300

    Create test_appJoaquin.py

---
+++ test_appJoaquin.py
@@ -0,0 +1,24 @@
+import unittest
+from appJoaquin import appJoaquin
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        # Configura el cliente de prueba
+        self.app = app.test_client()
+
+    def test_lista_articulos_contiene_prueba1_y_prueba2(self):
+        # Realiza una solicitud GET a la ruta /lista_articulos
+        response = self.app.get('/lista_articulos')
+
+        # Verifica que la respuesta sea exitosa (código 200)
+        self.assertEqual(response.status_code, 200)
+
+        # Obtén el contenido HTML de la respuesta
+        html_content = response.get_data(as_text=True)
+
+        # Verifica que 'Prueba1' y 'Prueba2' estén en el contenido HTML
+        self.assertIn('Prueba 1', html_content)
+        self.assertIn('Prueba 2', html_content)
+
+if __name__ == '__main__':
+    unittest.main()
